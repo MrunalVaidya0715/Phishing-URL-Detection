6ff44b924c8a537c83cc1612262a51e9e638341b
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-04-06 13:00:57 +0530

    ok

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 #importing required libraries
 
-from flask import Flask, request, render_template
-import numpy as np
-import pandas as pd
-from sklearn import metrics 
-import warnings
-import pickle
-warnings.filterwarnings('ignore')
+
 from feature import FeatureExtraction
 
 file = open("pickle/model.pkl","rb")
--- feature.py
+++ feature.py
@@ -1,15 +1,3 @@
-import ipaddress
-import re
-import urllib.request
-from bs4 import BeautifulSoup
-import socket
-import requests
-from googlesearch import search
-import whois
-from datetime import date, datetime
-import time
-from dateutil.parser import parse as date_parse
-from urllib.parse import urlparse
 
 class FeatureExtraction:
     features = []
